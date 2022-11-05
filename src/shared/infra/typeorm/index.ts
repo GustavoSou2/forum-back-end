33e56aba95bdacb7d2f2df85037f4f5ca2323e48
GustavSou2 <gustavosocial1032@gmail.com>
2022-11-05 08:13:19 -0300

    Heroku

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -8,14 +8,12 @@ config({ silent: true });
 
 const databaseConfig = database();
 
-const port = Number(process.env.PORT) || 3000;
-
 const dataSource = new DataSource({
   migrationsTableName: 'migrations',
   migrationsRun: true,
   type: "mysql",
   host: "us-cdbr-east-06.cleardb.net",
-  port: port,
+  port: 3306,
   username: "beb80608431dfb",
   password: "af01bd07",
   database: "heroku_3072c51389dd840",
