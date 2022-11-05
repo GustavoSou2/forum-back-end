af515aca21f484016b33ed3b0325e24da5770604
GustavSou2 <gustavosocial1032@gmail.com>
2022-11-05 09:01:03 -0300

    Heroku

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,14 +1,17 @@
 import 'reflect-metadata';
 import express from "express";
+import cors from 'cors'
 import {routes} from "./routes/routes";
 import {createConnection} from "../typeorm";
 
 const app = express();
 
+
 createConnection()
     .then(() => console.log('Connected to database'))
     .catch(() => console.log('Cannot connect to database'));
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
